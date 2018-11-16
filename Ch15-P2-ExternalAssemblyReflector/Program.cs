f4854ccb58e2dad3e2554f87f9e0f2a67b5ace5e
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-16 18:34:53 +0500

    Project Created

---
+++ Ch15-P2-ExternalAssemblyReflector/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch15_P2_ExternalAssemblyReflector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
